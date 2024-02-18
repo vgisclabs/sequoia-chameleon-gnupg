43548a4b3eb7d59db55b78e1e7532b38ed5bf6c8
Justus Winter <justus@sequoia-pgp.org>
2024-02-18 16:29:26 +0100

    Move the algorithm parsing code to the babel module.

--- src/babel.rs
+++ src/babel.rs
@@ -167,8 +167,28 @@ impl FromStr for Fish<SymmetricAlgorithm> {
     type Err = anyhow::Error;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        crate::argparse::utils::parse_cipher(s)
-            .map(|v| Fish(v))
+        let sl = s.to_lowercase();
+        if sl.starts_with('s') {
+            if let Ok(a) = sl[1..].parse::<u8>() {
+                return Ok(a.into()).map(Fish);
+            }
+        }
+
+        match sl.as_str() {
+            "idea" => Ok(SymmetricAlgorithm::IDEA),
+            "3des" => Ok(SymmetricAlgorithm::TripleDES),
+            "cast5" => Ok(SymmetricAlgorithm::CAST5),
+            "blowfish" => Ok(SymmetricAlgorithm::Blowfish),
+            "aes" => Ok(SymmetricAlgorithm::AES128),
+            "aes192" => Ok(SymmetricAlgorithm::AES192),
+            "aes256" => Ok(SymmetricAlgorithm::AES256),
+            "twofish" => Ok(SymmetricAlgorithm::Twofish),
+            "twofish128" => Ok(SymmetricAlgorithm::Twofish),
+            "camellia128" => Ok(SymmetricAlgorithm::Camellia128),
+            "camellia192" => Ok(SymmetricAlgorithm::Camellia192),
+            "camellia256" => Ok(SymmetricAlgorithm::Camellia256),
+            _ => Err(anyhow::anyhow!("Unknown cipher algorithm {:?}", s)),
+        }.map(Fish)
     }
 }
 
@@ -208,8 +228,23 @@ impl FromStr for Fish<HashAlgorithm> {
     type Err = anyhow::Error;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        crate::argparse::utils::parse_digest(s)
-            .map(|v| Fish(v))
+        let sl = s.to_lowercase();
+        if sl.starts_with('h') {
+            if let Ok(a) = sl[1..].parse::<u8>() {
+                return Ok(a.into()).map(Fish);
+            }
+        }
+
+        match sl.as_str() {
+            "md5" => Ok(HashAlgorithm::MD5),
+            "sha1" => Ok(HashAlgorithm::SHA1),
+            "ripemd160" => Ok(HashAlgorithm::RipeMD),
+            "sha256" => Ok(HashAlgorithm::SHA256),
+            "sha384" => Ok(HashAlgorithm::SHA384),
+            "sha512" => Ok(HashAlgorithm::SHA512),
+            "sha224" => Ok(HashAlgorithm::SHA224),
+            _ => Err(anyhow::anyhow!("Unknown hash algorithm {:?}", s)),
+        }.map(Fish)
     }
 }
 
@@ -233,8 +268,20 @@ impl FromStr for Fish<CompressionAlgorithm> {
     type Err = anyhow::Error;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        crate::argparse::utils::parse_compressor(s)
-            .map(|v| Fish(v))
+        let sl = s.to_lowercase();
+        if sl.starts_with('z') {
+            if let Ok(a) = sl[1..].parse::<u8>() {
+                return Ok(a.into()).map(Fish);
+            }
+        }
+
+        match sl.as_str() {
+            "none" | "uncompressed" => Ok(CompressionAlgorithm::Uncompressed),
+            "zip" => Ok(CompressionAlgorithm::Zip),
+            "zlib" => Ok(CompressionAlgorithm::Zlib),
+            "bzip2" => Ok(CompressionAlgorithm::BZip2),
+            _ => Err(anyhow::anyhow!("Unknown compression algorithm {:?}", s)),
+        }.map(Fish)
     }
 }
 
